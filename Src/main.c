f404c960ab67c8b06a4051aae6a7b795424fa877
hoel-har <hoel-har@f0r2s18.paris.42.school>
2026-04-30 16:44:15 +0200

    Lexer: Tree creat a node for the pipe, the first left, and the no pipe case. Missing the right case

--- Src/main.c
+++ Src/main.c
@@ -164,24 +164,26 @@ int	value_from_list_to_tree(t_tree *branch, t_token **cmd, size_t count)
 	return (0);
 }
 
-// t_tree	*left_branch(/* t_tree **tree,  */t_token **cmd, size_t count) // commence depuis la **head de tree jusquqau count et la prochaine commande apres le pipe devient la nouvelle head
-// {
-// 	t_tree	*left;
-// 	left = malloc(sizeof(t_tree));
-// 	if (!left)
-// 		return (NULL);
-// 	left->av = malloc(sizeof(char *) * (count + 1));
-// 	if (!left->av)
-// 		return (free(left), NULL);
-// 	left->ac = count;
-// 	left->fds = 0;
-// 	left->left = NULL;
-// 	left->right = NULL;
-// 	what_is_ptype(left, ((*cmd)));
-// 	if (value_from_list_to_tree(left, cmd, count))
-// 		return(free_split(left->av) , free(left) /*, free tout les left(av) precedants  */ ,NULL);
-// 	return (left);
-// }
+t_tree	*left_branch(/* t_tree **tree,  */t_token **cmd, size_t count) // commence depuis la **head de tree jusquqau count et la prochaine commande apres le pipe devient la nouvelle head
+{
+	t_tree	*left;
+	left = malloc(sizeof(t_tree));
+	if (!left)
+		return (NULL);
+	left->av = malloc(sizeof(char *) * (count + 1));
+	if (!left->av)
+		return (free(left), NULL);
+	left->ac = count;
+	left->fds = 0;
+	left->left = NULL;
+	left->right = NULL;
+	what_is_ptype(left, ((*cmd)));
+	if (value_from_list_to_tree(left, cmd, count))
+		return(free_split(left->av) , free(left) /*, free tout les left(av) precedants  */ ,NULL);
+	return (left);
+}
+
+
 // void	actualise_cmd(t_token **cmd, size_t count)
 // {
 // 	t_token	*current;
@@ -291,14 +293,14 @@ bool	search_pipe(t_token **cmd, size_t *count)
 {
 	t_token	*current;
 	
-	count = 0;
+	*count = 0;
 	current = *cmd;
 	while (current->next)
 	{
 		if (current->type == PIPE)
 			return (true);
 		current = current->next;
-		count += 1;
+		*count += 1;
 	}
 	if (current->type == PIPE)
 		return (ft_putstr_fd("Invalid command\n",1), false);
@@ -322,7 +324,7 @@ t_tree	*new_pipe(t_token **cmd, size_t *count)
 		return (free(branch_pipe), NULL);
 	branch_pipe->av[0] = ft_strdup("|");
 	if (!branch_pipe->av[0])
-		return (free(branch_pipe), free_split(branch_pipe->av), NULL);
+		return (free_split(branch_pipe->av), free(branch_pipe), NULL);
 	branch_pipe->av[1] = NULL;
 	branch_pipe->fds = 0;
 	branch_pipe->left = NULL;
@@ -330,19 +332,19 @@ t_tree	*new_pipe(t_token **cmd, size_t *count)
 	return (branch_pipe);
 }
 
-t_tree	*no_pipe_tree(t_token **cmd, size_t count)
+t_tree	*no_pipe_tree(t_token **cmd, size_t *count)
 {
 	t_tree	*branch_pipe;
-
 	branch_pipe = malloc(sizeof(t_tree));
 	if (!branch_pipe)
 		return (NULL);
+	search_pipe(cmd, count);
 	branch_pipe->parse_type = NO_PIPE_PARS;
-	branch_pipe->ac = count;
-	branch_pipe->av = malloc(sizeof(char *) * count + 1);
+	branch_pipe->ac = *count;
+	branch_pipe->av = malloc(sizeof(char *) * (*count + 1));
 	if (!branch_pipe->av)
 		return (free(branch_pipe), NULL);
-	value_from_list_to_tree(branch_pipe, cmd, count);
+	value_from_list_to_tree(branch_pipe, cmd, *count);
 	branch_pipe->fds = 0; //fonction pour calculer les fds
 	branch_pipe->left = NULL;
 	branch_pipe->right = NULL;
@@ -352,12 +354,21 @@ t_tree	*no_pipe_tree(t_token **cmd, size_t count)
 int	parser(t_tree *tree, t_token **cmd)
 {
 	size_t	count;
+	// t_token	*new_head;
 
 	count = 0;
 	if (!tree)
 		tree = new_pipe(cmd, &count);
 	if (!tree)
-		tree = no_pipe_tree(cmd, count);
+		tree = no_pipe_tree(cmd, &count);
+	if (!tree->left)
+		tree->left= left_branch(cmd, count);
+	if (!tree->left)
+		return (free_tree(tree), 1);
+	printf("value next node: %s\n", (*cmd)->next->value);
+	// new_head = (*cmd)->next;
+	// if (!tree->right)
+	// parser(tree->right, &new_head);
 	print_tree(tree);
 	return (0);
 }
